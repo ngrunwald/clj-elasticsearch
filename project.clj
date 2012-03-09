94429f17119a72ebe256330ecc107fb53999dd7a
Nils Grunwald <nils@grunwald.fr>
2012-03-09 20:28:47 +0100

    a lot more of usable working bits

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject clj-elasticsearch "0.0.1-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0-beta1"]
-                 [org.clojure/clojure-contrib "1.2.0-beta1"]
-                 [org.elasticsearch/elasticsearch "0.9.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.elasticsearch/elasticsearch "0.18.7"]
+                 [cheshire "3.0.0"]]
   :repositories { "sonatype.org" "http://oss.sonatype.org/content/repositories/releases/" })
--- src/clj_elasticsearch/client.clj
+++ src/clj_elasticsearch/client.clj
@@ -1,40 +1,140 @@
 (ns clj-elasticsearch.client
-  (:import (org.elasticsearch.node NodeBuilder)
-           (org.elasticsearch.common.xcontent XContentFactory)))
+  (:require [cheshire.core :as json])
+  (:import [org.elasticsearch.node NodeBuilder]
+           [org.elasticsearch.common.xcontent XContentFactory ToXContent$Params]
+           [org.elasticsearch.common.settings ImmutableSettings]
+           [org.elasticsearch.action.admin.indices.status IndicesStatusRequest]
+           [org.elasticsearch.common.io FastByteArrayOutputStream]
+           [org.elasticsearch.client.transport TransportClient]
+           [org.elasticsearch.common.transport InetSocketTransportAddress]))
 
-(def *client*)
+(def ^{:dynamic true} *client*)
+
+(defn update-settings-builder
+  ([builder settings]
+     (doseq [[k v] settings]
+       (println k v)
+       (if (or (vector? v) (list? v))
+         (.putArray builder (name k) (into-array String (map str v)))
+         (.put builder (name k) (str v))))
+     builder)
+  ([settings]
+     (update-settings-builder (ImmutableSettings/settingsBuilder) settings)))
 
 (defn make-node
-  [args]
-  (let [nodebuilder (NodeBuilder.)]
-    (. nodebuilder client true)
-    (if (:local args) (. nodebuilder local true))
-    (. nodebuilder node)))
+  [{:keys [local-mode client-mode load-config cluster-name settings hosts]
+    :or {client-mode true
+         load-config false
+         local-mode false
+         settings {}}
+    :as args}]
+  (let [nodebuilder (NodeBuilder.)
+        host-conf (if hosts {"discovery.zen.ping.unicast.hosts" hosts
+                             "discovery.zen.ping.multicast.enabled" false}
+                      {})]
+    (doto nodebuilder
+      (.client client-mode)
+      (.local local-mode)
+      (.loadConfigSettings load-config))
+    (if cluster-name
+      (.clusterName nodebuilder cluster-name))
+    (update-settings-builder (.settings nodebuilder) (merge settings host-conf))
+    (.node nodebuilder)))
 
-(defn get-client
-  [node]
-  (. node client))
+(defn make-inet-address
+  [spec]
+  (let [m (re-matcher #"([^\[\:]+)[\[\:]?(\d*)" spec)
+        _ (.find m)
+        [_ host p] (re-groups m)
+        port (if p (Integer/parseInt (str p)) 9300)]
+    (println host port)
+    (InetSocketTransportAddress. host port)))
 
-(defmacro with-server
+(defn make-transport-client
+  [{:keys [load-config cluster-name settings hosts sniff]
+    :or {client-mode true
+         load-config false
+         local-mode false
+         settings {}
+         sniff true}
+    :as args}]
+  (let [settings (if cluster-name (assoc settings "cluster.name" cluster-name) settings)
+        conf (update-settings-builder (merge settings {"client.transport.sniff" sniff}))
+        client (TransportClient. conf load-config)]
+    (doseq [host hosts]
+      (.addTransportAddress client (make-inet-address host)))
+    client))
+
+(defn make-content-builder
+  [& [type]]
+  (case type
+    :json (XContentFactory/jsonBuilder)
+    :smile (XContentFactory/smileBuilder)
+    (XContentFactory/smileBuilder)))
+
+(defn convert-response
+  [response]
+  (let [os (FastByteArrayOutputStream.)
+        builder (XContentFactory/smileBuilder os)
+        funny-request (proxy [ToXContent$Params] [])]
+    (.startObject builder)
+    (.toXContent response builder funny-request)
+    (.endObject builder)
+    (.flush builder)
+    (json/decode-smile (.underlyingBytes os) true)))
+
+(defn make-client
+  [type spec]
+  (case type
+    :node (.client (make-node spec))
+    :transport (make-transport-client spec)
+    (make-transport-client spec)))
+
+(defmacro with-node-client
   [server-spec & body]
   `(with-open [node# (make-node ~server-spec)]
-    (binding [*client* (get-client node#)]
+    (binding [clj-elasticsearch.client/*client* (.client node#)]
       (do
-        ~@body))))  
+        ~@body))))
+
+(defmacro with-transport-client
+  [server-spec & body]
+  `(with-open [client# (make-client :transport ~server-spec)]
+    (binding [clj-elasticsearch.client/*client* client#]
+      (do
+        ~@body))))
 
 (defn build-document
-  [builder doc]
-  (.startObject builder)
-  (doseq [[field value] doc]
-    (.field builder (name field) value))
-  (.endObject builder))
-
-(defn make-xson
   [doc]
-  (build-document (XContentFactory/xsonBuilder) doc))
+  (let [builder (XContentFactory/smileBuilder)]
+    (.startObject builder)
+    (doseq [[field value] doc]
+      (.field builder (name field) value))
+    (.endObject builder)))
 
-(defn make-json
-  [doc]
-  (build-document (XContentFactory/jsonBuilder) doc))
+(defn get-index-admin-client
+  [client]
+  (-> client (.admin) (.indices)))
 
+(defn get-cluster-admin-client
+  [client]
+  (-> client (.admin) (.cluster)))
+
+(defn index-status
+  ([client {:keys [indices snapshot? threaded? recovery? cb]
+            :or {snapshot? false
+                 threaded? true
+                 recovery? false}}]
+     (let [i (get-index-admin-client client)
+           req (if indices (IndicesStatusRequest. (into-array (map name indices)))
+                   (IndicesStatusRequest.))]
+       (doto req
+         (.snapshot snapshot?)
+         (.recovery recovery?)
+         (.listenerThreaded threaded?))
+       (if cb
+         (.status i req cb)
+         (.actionGet (.status i req)))))
+  ([args]
+     (index-status *client* args)))
 
