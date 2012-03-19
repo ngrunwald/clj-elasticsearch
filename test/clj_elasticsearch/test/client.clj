0b7bbbec8157c4cc38d1ebca72d1ce0daa0a93cd
Nils Grunwald <nils@grunwald.fr>
2012-03-19 13:45:18 +0100

    added tests

---
+++ test/clj_elasticsearch/test/client.clj
@@ -0,0 +1,41 @@
+(ns clj-elasticsearch.test.client
+  (:use [clj-elasticsearch.client] :reload-all)
+  (:use [clojure.test]
+        [clojure.java.io]
+        [cheshire.core]))
+
+(defn delete-dir
+  [path]
+  (let [d (file path)]
+    (if (.isDirectory d)
+      (doseq [f (seq (.listFiles d))]
+        (delete-dir f)
+        (try
+          (.delete f)
+          (catch Exception e
+            nil)))
+      (.delete d))))
+
+(defn es-fixture
+  [f]
+  (let [node (make-node {:local-mode true :client-mode false})]
+    (with-node-client {:local-mode true}
+      (f))
+    (.close node)
+    (delete-dir "data")))
+
+(use-fixtures :each es-fixture)
+
+(def match-all
+  (generate-string {"query" {"query_string" {"query" "*:*"}}}))
+
+(deftest es-client
+  (is (:id (index-doc {:index "test" :type "tyu"
+                       :source (build-document {:field1 "toto" :field2 42})})))
+  (is (> (:successful-shards (refresh-index {:indices ["test"]})) 0))
+  (is (= 1 (:count (count-docs {:indices ["test"]}))))
+  (is (get-in (first
+               (get-in (search {:indices ["test"] :types ["tyu"] :extra-source match-all})
+                       [:hits :hits]))
+              [:_source :field1])
+      "toto"))
--- test/clj_elasticsearch/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns clj-elasticsearch.test.core
-  (:use [clj-elasticsearch.core] :reload-all)
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
