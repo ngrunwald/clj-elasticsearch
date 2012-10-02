b84d7e113afddb5db2ad92e4c11905770432b3c5
Nils Grunwald <nils@grunwald.fr>
2012-10-02 17:46:53 +0200

    up version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clj-elasticsearch "0.2.1-SNAPSHOT"
+(defproject clj-elasticsearch "0.3.0"
   :description "Native Java API client wrapper for Elasticsearch"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [cheshire "4.0.0"]
+                 [cheshire "4.0.3"]
                  [gavagai "0.1.0"]]
   :profiles {:dev {:dependencies [[codox "0.5.0"]
                                   [org.elasticsearch/elasticsearch "0.19.9"]]}}
