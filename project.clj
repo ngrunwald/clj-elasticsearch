58fc3d19e5b108908fb74f8340fd055480223f5d
Nils Grunwald <nils@grunwald.fr>
2012-11-20 15:19:02 +0100

    bump version for release

--- project.clj
+++ project.clj
@@ -1,11 +1,13 @@
-(defproject clj-elasticsearch "0.3.2-SNAPSHOT"
+(defproject clj-elasticsearch "0.3.2"
   :description "Native Java API client wrapper for Elasticsearch"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [cheshire "4.0.3"]
-                 [gavagai "0.1.1-SNAPSHOT"]]
-  :profiles {:dev {:dependencies [[codox "0.5.0"]
-                                  [org.elasticsearch/elasticsearch "0.19.11"]]}}
-  :dev-dependencies [[codox "0.5.0"]
+                 [cheshire "4.0.4"]
+                 [gavagai "0.2.0"]]
+  :plugins [[codox "0.6.3"]]
+  :profiles {:dev {:dependencies [[org.elasticsearch/elasticsearch "0.19.11"]]}}
+  :dev-dependencies [[codox "0.6.3"]
                      [org.elasticsearch/elasticsearch "0.19.11"]]
-  :repositories { "sonatype.org" "http://oss.sonatype.org/content/repositories/releases/" }
-  :url "https://github.com/ngrunwald/clj-elasticsearch")
+  :repositories {"sonatype.org" "http://oss.sonatype.org/content/repositories/releases/"}
+  :url "https://github.com/ngrunwald/clj-elasticsearch"
+  :codox {:src-dir-uri "https://github.com/ngrunwald/clj-elasticsearch/tree/master"
+          :src-linenum-anchor-prefix "L"})
