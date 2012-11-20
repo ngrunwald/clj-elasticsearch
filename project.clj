6f3ff23b82d39a536e898aa552a8cf671b172630
Nils Grunwald <nils@grunwald.fr>
2012-11-20 15:28:23 +0100

    update version to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-elasticsearch "0.3.2"
+(defproject clj-elasticsearch "0.3.3-SNAPSHOT"
   :description "Native Java API client wrapper for Elasticsearch"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [cheshire "4.0.4"]
