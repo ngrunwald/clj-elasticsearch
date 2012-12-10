40c9a0d86069f9d3ad8abf191a4dc1896ec7e303
Nils Grunwald <nils@grunwald.fr>
2012-12-10 15:06:52 +0100

    release 0.3.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-elasticsearch "0.3.3-SNAPSHOT"
+(defproject clj-elasticsearch "0.3.3"
   :description "Native Java API client wrapper for Elasticsearch"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [cheshire "5.0.0"]
